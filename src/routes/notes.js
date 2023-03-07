a723a24ae1c3d4eb5b0063f475442fdc07d5afd7
Talla Giridhar <giridhar.talla2002@gmail.com>
2023-03-07 21:44:39 +0530

    aadd client

---
+++ src/routes/notes.js
@@ -0,0 +1,70 @@
+const router = require('express').Router()
+const { isAuth } = require('../utils/isAuth')
+const Notes = require('../models/notes')
+
+router.get('/', isAuth, async (req, res) => {
+	try {
+		const notes = await Notes.find({ user_id: req.user.id })
+		res.json(notes)
+	} catch (error) {
+		return res.status(500).json({ msg: error.message })
+	}
+})
+
+router.post('/', isAuth, async (req, res) => {
+	try {
+		// 1. Create a note
+		const { title, content, date } = req.body
+		const newNote = new Notes({
+			title,
+			content,
+			date,
+			user_id: req.user.id,
+			name: req.user.name,
+		})
+
+		// 2. Save note to database
+		await newNote.save()
+		res.status(200).json({ msg: 'Notes created. ✌', type: 'success' })
+	} catch (error) {
+		return res.status(500).json({ msg: error.message })
+	}
+})
+
+router.get('/:id', isAuth, async (req, res) => {
+	try {
+		const note = await Notes.findById(req.params.id)
+		res.json(note)
+	} catch (error) {
+		return res.status(500).json({ msg: error.message })
+	}
+})
+
+router.put('/:id', isAuth, async (req, res) => {
+	try {
+		const { title, content, date } = req.body
+		await Notes.findOneAndUpdate(
+			{ _id: req.params.id },
+			{
+				title,
+				content,
+				date,
+			}
+		)
+
+		res.json({ msg: 'Notes updated 🎉', type: 'info' })
+	} catch (error) {
+		return res.status(500).json({ msg: error.message })
+	}
+})
+
+router.delete('/:id', isAuth, async (req, res) => {
+	try {
+		await Notes.findByIdAndDelete(req.params.id)
+		res.json({ msg: 'Note Deleted', type: 'success' })
+	} catch (error) {
+		return res.status(500).json({ msg: error.message })
+	}
+})
+
+module.exports = router
