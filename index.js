f0e4ea4a865d205a3a929dea915073a45c801470
Talla Giridhar <giridhar.talla2002@gmail.com>
2023-03-07 21:46:19 +0530

    add src app

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require('./src/app')
+const logger = require('./src/utils/logger')
+const { PORT } = require('./src/utils/config')
+
+app.listen(PORT, (err) => {
+	if (err) {
+		logger.error(err?.message)
+	}
+	logger.info(`🚀 Listening on port ${PORT}`)
+})
---
+++ src/app.js
@@ -0,0 +1,37 @@
+const cors = require('cors')
+const express = require('express')
+const { connect } = require('mongoose')
+const cookieParser = require('cookie-parser')
+
+// Routes
+
+const { MONGO_URI } = require('./utils/config.js')
+const logger = require('./utils/logger')
+
+const app = express()
+
+// middleware functions
+app.use((_req, res, next) => {
+	res.setHeader('Access-Control-Allow-Origin', '*')
+	res.setHeader(
+		'Access-Control-Allow-Methods',
+		'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+	)
+	res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+	next()
+})
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use(cookieParser())
+// app.use(cors())
+
+// connecting to mongodb atlas
+connect(MONGO_URI, {
+	useNewUrlParser: true,
+	useCreateIndex: true,
+	useUnifiedTopology: true,
+})
+	.then(() => logger.info('MongoDB connection is established successfully! 🎉'))
+	.catch((err) => logger.error('Something went wrong: ' + err?.message))
+
+module.exports = app
