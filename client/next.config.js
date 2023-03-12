1cfb8d0b727a91a9afca6b5f779681b3ed6ea08d
Talla Giridhar <giridhar.talla2002@gmail.com>
2023-03-12 16:36:51 +0530

    update layout

--- client/next.config.js
+++ client/next.config.js
@@ -1,9 +1,20 @@
-/** @type {import('next').NextConfig} */
-const withPWA = require('next-pwa')({
-	dest: 'public',
-	register: true,
-	disable: process.env.NODE_ENV === 'development',
-	skipWaiting: true,
-})
+// /** @type {import('next').NextConfig} */
+// const withPWA = require('next-pwa')({
+// 	dest: 'public',
+// 	register: true,
+// 	disable: process.env.NODE_ENV === 'development',
+// 	skipWaiting: true,
+// })
 
-module.exports = withPWA()
+// module.exports = withPWA()
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+	output: 'export',
+	images: {
+		unoptimized: true,
+	},
+}
+
+module.exports = nextConfig
